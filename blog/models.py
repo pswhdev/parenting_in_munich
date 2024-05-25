89badd3e14b64c86efda332aef5eb17d7b2d25fc
pswhdev <patriciaswhalley@gmail.com>
2024-05-25 14:07:20 +0200

    Add Category model

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
 
-
-
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     STATUS = (
