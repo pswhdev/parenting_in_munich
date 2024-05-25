0598e27793ad9ec62a983b2809bd333f0b15d725
pswhdev <patriciaswhalley@gmail.com>
2024-05-25 14:04:21 +0200

    Create Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+class Post(models.Model):
+    STATUS = (
+        (0, 'Draft'),
+        (1, 'Published')
+        )
+    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=False)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
