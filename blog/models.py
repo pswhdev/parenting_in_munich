bb4279724d62f536a65faf822bdeb3ee3fadf6c0
pswhdev <patriciaswhalley@gmail.com>
2024-05-25 16:00:10 +0200

    Add username to the Post model

--- blog/models.py
+++ blog/models.py
@@ -14,14 +14,22 @@ class Post(models.Model):
         (1, 'Published')
         )
     
-    user = models.ForeignKey(User, on_delete=models.PROTECT, null=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    username = models.CharField(max_length=150, null=True, blank=True)
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    # To override the save method to set the username field
+    # so that if a user is deleted we can still see who wrote it
+    def save(self, *args, **kwargs):
+        if self.user and not self.username:
+            self.username = self.user.username
+        # Call the superclass's save method to save the object to the DB
+        super().save(*args, **kwargs)    
 
     def __str__(self):
         return self.title
