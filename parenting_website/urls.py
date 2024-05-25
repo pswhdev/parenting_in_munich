a54ed7a227f19a5c9096b0dbb8271c9820201c04
pswhdev <patriciaswhalley@gmail.com>
2024-05-25 11:30:04 +0200

    Create blog app

--- parenting_website/urls.py
+++ parenting_website/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import site_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', site_blog, name ='blog'),
 ]
