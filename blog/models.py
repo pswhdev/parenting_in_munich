993ad7c84c9c344a52a849d68b2e7b4c8114d0cd
pswhdev <patriciaswhalley@gmail.com>
2024-05-25 19:33:13 +0200

    Add signals to models to retain comments on user deletion

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 # Create your models here.
 class Category(models.Model):
@@ -14,7 +16,9 @@ class Post(models.Model):
         (1, 'Published')
         )
     
-    user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="blog_posts")
+    # To allow the post to remain on the database and the site
+    # even if the user is deleted
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="blog_posts")
     author = models.CharField(max_length=150, null=True, blank=True)
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
@@ -23,15 +27,12 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     excerpt = models.TextField(blank=True)
-    
-    # To override the save method to set the username field
-    # so that if a user is deleted we can still see who wrote it
+
     def save(self, *args, **kwargs):
         if self.user and not self.author:
             self.author = self.user.username
-        # Call the superclass's save method to save the object to the DB
-        super().save(*args, **kwargs)    
-
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
@@ -41,7 +42,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='commenter')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='commenter')
     author = models.CharField(max_length=150, null=True, blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -49,12 +50,31 @@ class Comment(models.Model):
     def save(self, *args, **kwargs):
         if self.user and not self.author:
             self.author = self.user.username
-        # Call the superclass's save method to save the object to the DB
-        super().save(*args, **kwargs) 
+        super().save(*args, **kwargs)
 
     def __str__(self):
-
         return f"Comment by {self.author} on {self.post.title}"
     
     class Meta:
         ordering = ["created_on"]
+
+
+# Signal to update the author field before deleting the user
+# in order to keep the integrity of the database
+@receiver(pre_delete, sender=User)
+def update_author_before_user_delete(sender, instance, **kwargs):
+    deactivated_username = f"{instance.username} [Deactivated]"
+    
+    # Update Post author field
+    posts = Post.objects.filter(user=instance)
+    for post in posts:
+        post.author = deactivated_username
+        post.save()
+
+    # Update Comment author field
+    comments = Comment.objects.filter(user=instance)
+    for comment in comments:
+        comment.author = deactivated_username
+        comment.save()
+
+
