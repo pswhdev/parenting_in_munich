dee4edc14eddcad085c258727cfd5b3457299064
pswhdev <patriciaswhalley@gmail.com>
2024-05-25 17:24:52 +0200

    Update __str__ on Post to include author

--- blog/models.py
+++ blog/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
         super().save(*args, **kwargs)    
 
     def __str__(self):
-        return self.title
+        return f"{self.title} | written by {self.author}"
 
     class Meta:
         ordering = ["-created_on"]
