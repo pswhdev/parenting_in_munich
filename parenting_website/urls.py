974457d1fc6e081d37c6b841199e3df857b7b56b
pswhdev <patriciaswhalley@gmail.com>
2024-05-26 17:08:58 +0200

    Add blog views

--- parenting_website/urls.py
+++ parenting_website/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import site_blog
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', site_blog, name ='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
