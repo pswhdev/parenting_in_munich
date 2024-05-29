0eae9b903925dcf6153978450a83723ea496e3e2
pswhdev <patriciaswhalley@gmail.com>
2024-05-29 06:29:30 +0200

    Switch database for deployment

--- parenting_website/settings.py
+++ parenting_website/settings.py
@@ -84,19 +84,19 @@ WSGI_APPLICATION = 'parenting_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # To be used when deployed 
 
-# DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
+DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 
