8148470979023c931668cf0d7d0de47c659ca0c4
pswhdev <patriciaswhalley@gmail.com>
2024-05-29 11:16:33 +0200

    Fix database issue

---
+++ drop_all_tables.sql
@@ -0,0 +1,5 @@
+                     ?column?                      
+---------------------------------------------------
+ DROP TABLE IF EXISTS "django_migrations" CASCADE;
+(1 row)
+
--- parenting_website/settings.py
+++ parenting_website/settings.py
@@ -85,18 +85,21 @@ WSGI_APPLICATION = 'parenting_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-if os.getenv('DATABASE_URL'):
-    DATABASES = {
+DATABASES = {
         'default': dj_database_url.parse(os.environ['DATABASE_URL'])
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+# if os.getenv('DATABASE_URL'):
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ['DATABASE_URL'])
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 CSRF_TRUSTED_ORIGINS = [
