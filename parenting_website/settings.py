aa49226851a68de1f5c768d62cbad1908545d719
pswhdev <patriciaswhalley@gmail.com>
2024-06-02 14:08:30 +0200

    Add style and link to categories to posts.html

--- parenting_website/settings.py
+++ parenting_website/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.static",
                 "django.contrib.messages.context_processors.messages",
                 
                 # My custom processor
