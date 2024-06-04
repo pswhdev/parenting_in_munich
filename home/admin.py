30727dbcc48da24d5a36590d5ae4b438d026159f
pswhdev <patriciaswhalley@gmail.com>
2024-06-05 00:33:22 +0200

    Create admin class in admin.py home

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import HeroSection
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
-admin.site.register(HeroSection)
+@admin.register(HeroSection)
+class HeroSectionAdmin(SummernoteModelAdmin):
+    
+    summernote_fields = ('text',)
