b22ef9a3184ff7219aeaacb85d35868187587be1
pswhdev <patriciaswhalley@gmail.com>
2024-06-05 00:22:05 +0200

    Add HeroSection model in the home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+
+class HeroSection(models.Model):
+    featured_image = CloudinaryField('image', default='placeholder')
+    text = models.TextField()
+
+    def __str__(self):
+        return "Hero Section"
