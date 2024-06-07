8618313ea8156e9b6c61bc4d5181739e3e1ee4b4
pswhdev <patriciaswhalley@gmail.com>
2024-06-07 10:32:48 +0200

    Change signals settings to use allauth

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,11 +1,9 @@
-from django.db.models.signals import post_save
+from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
+@receiver(user_signed_up, sender=User)
+def create_profile_on_signup(request, user, **kwargs):
+    Profile.objects.create(user=user)
