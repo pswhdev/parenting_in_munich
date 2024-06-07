d23108f02637ac750db076586cf6d199188564f3
pswhdev <patriciaswhalley@gmail.com>
2024-06-07 11:46:33 +0200

    Create management commands to add profile for admin user

---
+++ accounts/management/commands/create_profiles.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from accounts.models import Profile
+
+
+class Command(BaseCommand):
+    help = 'Create profiles for users who do not have one'
+
+    def handle(self, *args, **kwargs):
+        users_without_profiles = User.objects.filter(profile__isnull=True)
+        for user in users_without_profiles:
+            Profile.objects.create(user=user)
+            self.stdout.write(self.style.SUCCESS('Profile created for '
+                                                 f'user {user.username}'))
