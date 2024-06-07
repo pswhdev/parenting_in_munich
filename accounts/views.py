8a544307cf8b3097c5cd07a520a1dc16a4bdee8e
pswhdev <patriciaswhalley@gmail.com>
2024-06-07 12:26:16 +0200

    Add go back button to delete_profile

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ def profile(request):
             user_form.save()
             profile_form.save()
             messages.success(request, 'Your profile has been updated!')
-            return redirect('profile')
+            return redirect('user:profile')
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
