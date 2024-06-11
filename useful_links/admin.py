acac6d9f3d180a6dc849a3776caf205ca3ac53d8
pswhdev <patriciaswhalley@gmail.com>
2024-06-11 11:44:35 +0200

    Add unique=True for links and link title

--- useful_links/admin.py
+++ useful_links/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from .models import Topic, Link
 
+# allows to add and edit Link objects directly within the Topic admin page
 class LinkInline(admin.TabularInline):
     model = Link
     extra = 1
+    # Order links by title alphabetically in the inline form
+    ordering = ['title']
 
 class TopicAdmin(admin.ModelAdmin):
     inlines = [LinkInline]
     # Adding search fields for autocomplete
     search_fields = ('name',)
+    # Order topics by name alphabetically in the admin interface
+    ordering = ['name']
 
 class LinkAdmin(admin.ModelAdmin):
     list_display = ('title', 'url', 'topic')
@@ -16,6 +21,8 @@ class LinkAdmin(admin.ModelAdmin):
     list_filter = ('topic',)
     # To autocomplete for topic field
     autocomplete_fields = ['topic']
+    # Order links by title alphabetically in the admin interface
+    ordering = ['title']
 
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(Link, LinkAdmin)
--- useful_links/models.py
+++ useful_links/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
-
 class Topic(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+    # To order topics alphabetically by name on the useful_links page
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
 
-
 class Link(models.Model):
-    topic = models.ForeignKey(
-        Topic, related_name='links', on_delete=models.CASCADE
-        )
-    title = models.CharField(max_length=200)
-    url = models.URLField()
+    topic = models.ForeignKey(Topic, related_name='links', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, unique=True)
+    url = models.URLField(unique=True)
+
+    # To order links alphabetically by title on the useful_links page
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return self.title
