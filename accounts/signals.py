b5887b1d84662fdd53a11eb95e88f0a31cef14df
pswhdev <patriciaswhalley@gmail.com>
2024-06-12 13:05:27 +0200

    Add signal to accounts to create profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,9 +1,17 @@
 from allauth.account.signals import user_signed_up
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
-
+# Creates a profile when a user signs up via allauth
 @receiver(user_signed_up, sender=User)
 def create_profile_on_signup(request, user, **kwargs):
     Profile.objects.create(user=user)
+
+# Creates a profile when a new User instance is saved
+# using django for create superusers for instance
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
