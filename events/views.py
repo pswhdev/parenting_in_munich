aac8df691b0795f9575cd68c872e932a0ac20af4
pswhdev <patriciaswhalley@gmail.com>
2024-06-16 10:27:28 +0200

    Add filter to order events in events views

--- events/views.py
+++ events/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from .models import Event
 
 
 @login_required
 def events(request):
-    events_list = Event.objects.all()
-    return render(request, 'events/events.html', {'events': events_list})
+    today = timezone.now().date()
+    events_list = Event.objects.filter(
+        end_date__gte=today).order_by("start_date")
+    return render(request, "events/events.html", {"events": events_list})
 
 
 def restricted_area(request):
-    return render(request, 'events/restricted_area.html')
+    return render(request, "events/restricted_area.html")
